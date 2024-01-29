171dc98772372a9bcacaed248e79fb4fba529edb
itsaFan <steffansim@gmail.com>
2024-01-30 05:38:29 +0800

    add pagination

--- src/pages/manage-fishes-page.jsx
+++ src/pages/manage-fishes-page.jsx
@@ -5,12 +5,24 @@ import FishItem from "../components/fish/fish-item";
 import SearchFish from "../components/fish/search-fish";
 import AddFish from "../components/fish/add-fish-modal";
 import { getAllTypes } from "../api/type-api";
+import FishPagination from "../components/fish/fish-pagination";
 
 export default function ManageFishPage() {
   const [fishes, setFishes] = useState([]);
   const [types, setTypes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const ITEMS_PER_PAGE = 5;
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(fishes.length / ITEMS_PER_PAGE);
+  const indexOfLastItem = currentPage * ITEMS_PER_PAGE;
+  const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE;
+  const currentFishes = fishes.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   console.log(fishes);
 
   const fetchFishes = useCallback(async () => {
@@ -18,7 +30,7 @@ export default function ManageFishPage() {
     try {
       const data = await getAllFishes();
       const types = await getAllTypes();
-      setFishes(data.data);
+      setFishes(data.data.reverse());
       setTypes(types.data);
     } catch (error) {
       console.error("Failed to fetch fishes or types:", error);
@@ -35,19 +47,18 @@ export default function ManageFishPage() {
     fetchFishes();
   };
 
-  const reversedFishes = [...fishes].reverse();
-
   return (
     <>
-      <section className="section-wrapper">
+      <section id="fish-section" className="section-wrapper">
         <div className="flex flex-wrap justify-between items-center mb-3">
           <button className="bg-rose-500 text-white px-3 py-2.5 rounded-md" onClick={() => setIsOpen(true)}>
             Add Fish
           </button>
           <SearchFish />
         </div>
+        <FishPagination totalPages={totalPages} paginate={paginate} currentPage={currentPage} />
         <ContentWrapper loading={loading}>
-          {reversedFishes.map((fish) => (
+          {currentFishes.map((fish) => (
             <FishItem key={fish._id} {...fish} reFetchFishes={reFetchFishes} typesData={types} />
           ))}
         </ContentWrapper>
