64926cdd0882befb56c16dfb53fd6c27d24c38bd
itsaFan <steffansim@gmail.com>
2024-01-29 12:26:05 +0800

    drawer

--- src/components/layout/drawer-nav.jsx
+++ src/components/layout/drawer-nav.jsx
@@ -1,20 +1,42 @@
+import { LogOut } from "lucide-react";
+import { Settings } from "lucide-react";
+import { Menu } from "lucide-react";
+
 export default function SideDrawer() {
   return (
     <div className="drawer">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
-          Open drawer
+        <label htmlFor="my-drawer" className="flex transition duration-200 hover:text-rose-800">
+          <Menu size={30} />
         </label>
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <ul className="menu p-4 w-80 min-h-full bg-white text-gray-700">
+          <span className="font-bold tracking-wider text-lg">Menu</span>
+          <div className="w-full h-[2px] bg-gray-300 opacity-30 my-2" />
           <li>
-            <a>Sidebar Item 1</a>
+            <a>Manage Fish</a>
           </li>
           <li>
-            <a>Sidebar Item 2</a>
+            <a>Manage Discount</a>
+          </li>
+          <li>
+            <a>Manage Schedule</a>
+          </li>
+          <div className="w-full h-[2px] bg-gray-300 opacity-30 mt-5" />
+          <li>
+            <a>
+              <Settings size={18} />
+              Settings
+            </a>
+          </li>
+          <li>
+            <a>
+              <LogOut size={18} />
+              Logout
+            </a>
           </li>
         </ul>
       </div>
--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -4,8 +4,11 @@ import SideDrawer from "./drawer-nav";
 export default function Layout() {
   return (
     <>
-      <header className="sticky h-heading top-0 z-20 flex justify-between items-center py-2 px-4 md:px-8 font-normal h-full bg-gray-100 shadow">
-        <SideDrawer />
+      <header className="sticky h-heading top-0 z-20 flex justify-between items-center py-2 px-4 md:px-8 font-normal h-full bg-gray-100 shadow text-rose-500">
+        <div>
+          <SideDrawer />
+        </div>
+        <span className="font-semibold tracking-wide text-2xl">Admin</span>
       </header>
       <div className="section-wrapper">
         <Outlet />
