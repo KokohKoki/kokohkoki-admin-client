1715b6a52248492a2443a5533690a64a32beb938
itsaFan <steffansim@gmail.com>
2024-02-03 15:11:55 +0800

    fix styling - refetch token

--- src/components/auth/update-user.jsx
+++ src/components/auth/update-user.jsx
@@ -4,24 +4,26 @@ import { useAuth } from "../../context/use-context";
 import ReactDOM from "react-dom";
 import classes from "./scss/auth.module.scss";
 import { useEffect, useState } from "react";
-import { updateUser } from "../../api/auth-api";
+import { loginApi, updateUser } from "../../api/auth-api";
 import UpdateUserForm from "./forms/edit-user-form";
 
 export default function UpdateUser({ isOpen, setIsOpen, currentUsername, userId }) {
   const { userToken } = useAuth();
   const [error, setError] = useState("");
-  const [username, setUsername] = useState(currentUsername);
+  const [username, setUsername] = useState(currentUsername || "");
   const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await updateUser(userToken, userId, username, password);
+      const newToken = await loginApi(username, password);
+      sessionStorage.setItem("userToken", newToken);
+      setIsOpen(false);
+      window.location.reload()
     } catch (error) {
       console.error("Failed to edit user:", error);
       setError(error.response?.data.message);
-    } finally {
-      setIsOpen(false);
     }
   };
 
