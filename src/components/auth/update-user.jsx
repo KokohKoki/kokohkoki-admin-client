e23f385a8413023d68e5f228e1584f8ac45811ef
itsaFan <steffansim@gmail.com>
2024-02-03 15:31:00 +0800

    fix undefined

--- src/components/auth/update-user.jsx
+++ src/components/auth/update-user.jsx
@@ -7,11 +7,17 @@ import { useEffect, useState } from "react";
 import { loginApi, updateUser } from "../../api/auth-api";
 import UpdateUserForm from "./forms/edit-user-form";
 
-export default function UpdateUser({ isOpen, setIsOpen, currentUsername, userId }) {
-  const { userToken } = useAuth();
+export default function UpdateUser({ isOpen, setIsOpen, userId }) {
+  const { userToken, userPayload } = useAuth();
   const [error, setError] = useState("");
-  const [username, setUsername] = useState(currentUsername || "");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (userPayload?.username) {
+      setUsername(userPayload.username);
+    }
+  }, [userPayload]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,7 +26,7 @@ export default function UpdateUser({ isOpen, setIsOpen, currentUsername, userId 
       const newToken = await loginApi(username, password);
       sessionStorage.setItem("userToken", newToken);
       setIsOpen(false);
-      window.location.reload()
+      window.location.reload();
     } catch (error) {
       console.error("Failed to edit user:", error);
       setError(error.response?.data.message);
