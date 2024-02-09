7802aecef7b5aec30a2f899a577f14364efe6c2a
itsaFan <steffansim@gmail.com>
2024-02-10 02:32:56 +0800

    add sorting

---
+++ src/components/fish/sorting-fishes.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+
+export default function SortFishes({ value, onChange }) {
+  return (
+    <div className="flex gap-1 items-center text-gray-600">
+      <label htmlFor="itemsPerPage">Show:</label>
+      <select id="itemsPerPage" value={value} onChange={onChange} className="my-2.5 px-2.5 py-1.5 text-sm bg-white w-fit rounded-lg ">
+        {[5, 10, 15, 20, 30].map((number) => (
+          <option key={number} value={number}>
+            {number}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
