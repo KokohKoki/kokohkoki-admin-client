a5157b97f835b965d5c75d944105da96ab132d3e
Muhammad Avicena <cenarahmant.dev@gmail.com>
2024-03-12 17:16:42 +0700

    feat: modify the icon kokohkoki

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 function Logo() {
-  return <img src="./logoKokohKokiWebsite.png" alt="logoKokohKoki" style={{ width: "60%", height: "auto" }} />;
+  return <img src="./LogoKokohKokiBulet.png" alt="logoKokohKoki" style={{ width: "60%", height: "auto" }} />;
 }
 
 export default Logo;
